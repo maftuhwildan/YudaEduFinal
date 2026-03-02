c514728cd09d942ca1f267f592b710cd21034540
maftuhwildan <maftuhwildan@gmail.com>
2026-03-02 23:43:47 +0700

    fix: simplify entrypoint to just start app, prisma db push done manually

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-set -e
-
-echo "==> Syncing database schema..."
-prisma db push --schema=./prisma/schema.prisma --accept-data-loss
-echo "==> Schema sync complete."
 
 echo "==> Starting application..."
 exec node server.js
